408bbd76f6a2bb9aa70a4a0b2dc09516727b2074
yzhen-li <60626955+yzhen-li@users.noreply.github.com>
2020-03-18 11:57:48 -0700

    remove download_url to use twine later

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ requirements = []
 
 setup(
     name="pyllama",
-    version="v0.0.5",
+    version="0.0.5",
     author="Yi Zhen",
     author_email="zhenyi@gmail.com",
     description="A package to machine learning algorithms",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/camel4all/pyllama",
-    download_url="https://github.com/camel4all/pyllama/archive/v0.0.5.tar.gz",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
